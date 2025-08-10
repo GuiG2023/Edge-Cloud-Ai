c95b2c046c64fe9f679b6e022fd14a6d91d412fd
Guiran <150613697+GuiG2023@users.noreply.github.com>
2025-08-09 20:57:06 -0700

    top 10

--- train_router.py
+++ train_router.py
@@ -286,7 +286,7 @@ if __name__ == "__main__":
         }
 
         # --- 实验选择开关 ---
-        EXPERIMENT_TO_RUN = "TOP_5"
+        EXPERIMENT_TO_RUN = "TOP_10"
         # ----------------------
 
         selected_features = feature_sets.get(EXPERIMENT_TO_RUN)
